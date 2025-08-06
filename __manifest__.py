9b62a379ba7a32a7a756a39aed3294d5686e2703
SistemasAYASA <126886766+SistemasAYASA@users.noreply.github.com>
2025-08-06 13:44:53 -0600

    Add __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'prueba53-addons',
+    'version': '18.0.1.0.0',
+    'category': 'Custom',
+    'summary': 'Custom Odoo module',
+    'description': '''
+        Custom Odoo module created with Odoo.sh Local
+    ''',
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+    ],
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
